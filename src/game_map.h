35d65f8ff6ab681d466f91ecb32449853d0841ba
Alex Muchau <muchau04@gmail.com>
2023-06-17 20:55:15 -0300

    available_pos

--- src/game_map.h
+++ src/game_map.h
@@ -7,6 +7,7 @@ class GameMap {
     int win_height;
     int win_width;
     WINDOW * game_win;
+    vector<vector<int>> av_positions;
     void instantiate_colors();
     void add_wall(const int& y, const int& x);
     void add2x2_wall(const int& yMin, const int& xMin);
@@ -19,6 +20,7 @@ class GameMap {
   public:
     GameMap(const int& game_screen_max, const int& opt, const double& start_y, const double& start_x);
     WINDOW * get_win();
+    vector<vector<int>> get_available_pos();
 };
 
 #endif
